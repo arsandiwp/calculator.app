1061430e6ee6ae6fe1734a01ae63b43cc6bab33e
arsandiwp <arsandiwp@gmail.com>
2020-03-11 12:12:31 +0700

    update terbaru

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Hasil Penambahan " + a + "+" + b + "=" + penambahan(a, b));
+        Console.WriteLine("HAsil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+
+        Console.WriteLine("\nTekan sembarangan key untuk keluar");
+        Console.ReadKey();
+
     }
+    static int penambahan (int a, int b)
+     {
+            return a + b;
+     }
+     static int pengurangan (int a, int b)
+     {
+            return a - b;
+     }
 }
